8a86e4c533e7250f59c045af53c338a3818ddb1c
Juan Felipe Alves Flores <vilaJJ@users.noreply.github.com>
2024-04-17 21:43:02 -0300

    :construction: feat: Criado arquivo AppWidget

--- dicionario_tupi/lib/src/app_widget.dart
+++ dicionario_tupi/lib/src/app_widget.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetMaterialApp(
+      title: 'Dicionário Tupi',
+    );
   }
 }
